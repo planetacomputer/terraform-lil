7583f6929959d68df2f5dcfcfb3e2bbe018e574d
planetacomputer <evapardeza123@gmail.com>
2020-05-30 17:37:51 +0200

    subnets aws

--- resources.tf
+++ resources.tf
@@ -1,8 +1,3 @@
-resource "google_compute_network" "our_development_network" {
-  name = "devnetwork"
-  auto_create_subnetworks = true
-}
-
 resource "aws_vpc" "environment-example-two" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
@@ -12,3 +7,28 @@ resource "aws_vpc" "environment-example-two" {
   }
 }
 
+resource "aws_subnet" "subnet1" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)}"
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "subnet2" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 2, 2)}"
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  availability_zone = "us-west-1b"
+}
+
+resource "aws_security_group" "subnetsecurity" {
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+
+  ingress {
+    cidr_blocks = [
+      "${aws_vpc.environment-example-two.cidr_block}"
+    ]
+
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+  }
+}
